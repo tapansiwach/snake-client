3ffe41cfb5b7de8871f5228dc266466470f2d234
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-25 04:09:54 -0400

    refactor: move connect function to a client.js file

---
+++ client.js
@@ -0,0 +1,18 @@
+// established a connection with the game server
+const connect = () => {
+  const client = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+
+  // interpret incoming data as text
+  client.setEncoding("utf8");
+
+  client.on("data", () => {
+    console.log("server says:", data);
+  });
+
+  return client;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -3,22 +3,7 @@ const net = require("net");
 const IP = "10.0.0.209";
 const PORT = "50541";
 
-// established a connection with the game server
-const connect = () => {
-  const client = net.createConnection({
-    host: IP,
-    port: PORT
-  });
-
-  // interpret incoming data as text
-  client.setEncoding("utf8");
-
-  client.on("data", () => {
-    console.log("server says:", data);
-  });
-
-  return client;
-};
+const connect = require("./client");
 
 console.log("Connecting...");
 connect();
