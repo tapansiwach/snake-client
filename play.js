eccefa8e5e55508e8bd22190c460c630039978b6
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-25 05:02:05 -0400

    setup stdin

--- play.js
+++ play.js
@@ -8,3 +8,20 @@ const { connect } = require("./client");
 console.log("Connecting...");
 connect();
 
+const handleUserInput = (key) => {
+  if (key === "\u0003") process.exit();
+};
+
+const setupStdin = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+setupStdin();
+
