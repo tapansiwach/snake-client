095c85a7075c635371b3a7005b7bbd569f2cc616
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-25 05:29:07 -0400

    setup wasd movement for snake

--- input.js
+++ input.js
@@ -1,8 +1,36 @@
+let connection;
+const MOVEUP = "Move: up";
+const MOVELEFT = "Move: left";
+const MOVEDOWN = "Move: down";
+const MOVERIGHT = "Move: right";
+
 const handleUserInput = (key) => {
   if (key === "\u0003") process.exit();
+
+  if (key === "w") {
+    // console.log(MOVEUP);
+    connection.write(MOVEUP);
+  }
+
+  if (key === "a") {
+    // console.log(MOVELEFT);
+    connection.write(MOVELEFT);
+  }
+
+  if (key === "s") {
+    // console.log(MOVVEDOWN);
+    connection.write(MOVEDOWN);
+  }
+
+  if (key === "d") {
+    // console.log(MOVERIGHT);
+    connection.write(MOVERIGHT);
+  }
 };
 
-const setupStdin = function() {
+const setupInput = function(conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,4 +41,4 @@ const setupStdin = function() {
   return stdin;
 }
 
-module.exports = { setupStdin };
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -4,10 +4,10 @@ const IP = "10.0.0.209";
 const PORT = "50541";
 
 const { connect } = require("./client");
-const { setupStdin } = require("./input");
+const { setupInput } = require("./input");
 
 console.log("Connecting...");
-connect();
+const conn = connect();
 
-setupStdin();
+setupInput(conn);
 
